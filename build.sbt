3a5b46db7e9fcdf5f49111270bd067d5c5a8dc19
alikemal.ocalan <alikemal.ocalan@trendyol.com>
2020-11-10 20:12:16 +0300

    Improve Scheduler

--- build.sbt
+++ build.sbt
@@ -4,35 +4,23 @@ version := "0.1.1"
 organization := "com.github.alikemalocalan"
 scalaVersion := "2.12.12"
 
-val AkkaVersion = "2.6.10"
-// To make the default compiler and REPL use Dotty
-//val dottyVersion = "0.27.0-RC1"
-//scalaVersion := dottyVersion
-
 resolvers in Global ++= Seq(
-  "apache-snapshots" at "https://repository.apache.org/snapshots/",
   "Typesafe" at "https://repo.typesafe.com/typesafe/releases/",
   "Sbt plugins" at "https://dl.bintray.com/sbt/sbt-plugin-releases",
   "instagram4j" at "https://dl.bintray.com/instagram4j/maven"
 )
 
-
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
-  "com.typesafe" % "config" % "1.4.1",
-  "io.spray" %% "spray-json" % "1.3.5",
   "com.github.instagram4j" % "instagram4j" % "develop-370fad2",
   "com.amazonaws" % "aws-java-sdk-s3" % "1.11.894",
+  "com.typesafe" % "config" % "1.4.1",
   "javax.xml.bind" % "jaxb-api" % "2.3.1",
-  "commons-io" % "commons-io" % "2.6"
+  "commons-io" % "commons-io" % "2.8.0"
 )
 
 mainClass in Compile := Some("com.github.alikemalocalan.instastorysaver.InstaStorySaver")
 
 scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8")
-
-// To cross compile with Dotty and Scala 2
-//crossScalaVersions := Seq(dottyVersion, scala213Version)
 
 val stage = taskKey[Unit]("Stage task")
 
--- src/main/scala/com/github/alikemalocalan/instastorysaver/InstaStorySaver.scala
+++ src/main/scala/com/github/alikemalocalan/instastorysaver/InstaStorySaver.scala
@@ -1,34 +1,30 @@
 package com.github.alikemalocalan.instastorysaver
 
-import akka.actor.ActorSystem
+import java.util.{Timer, TimerTask}
+
 import com.github.alikemalocalan.instastorysaver.service.InstaService
 import com.github.instagram4j.instagram4j.IGClient
 import org.apache.commons.logging.{Log, LogFactory}
 
-import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration._
 import scala.util.{Failure, Success, Try}
 
 object InstaStorySaver extends App with Config {
-  implicit val system: ActorSystem = ActorSystem("InstaStorySaver-Actor-System")
-  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+  val logger: Log = LogFactory.getLog(this.getClass)
+  val timer = new Timer()
 
   implicit val client: IGClient = InstaService
     .login(userName, passWord)
 
-  val logger: Log = LogFactory.getLog(this.getClass)
-
-  val saverScheduler: Runnable = () => {
-    Try {
-      logger.info("Starting Saving Stories...")
-      InstaService.saveStoriesToS3
-    } match {
-      case Success(_) => logger.info("Finish Success, yuu are the best!!!")
-      case Failure(exception) => logger.error("Feed error on: ", exception)
-    }
+  val saverScheduler: TimerTask = new TimerTask {
+    def run(): Unit =
+      Try {
+        logger.info("Starting Saving Stories...")
+        InstaService.saveStoriesToS3
+      } match {
+        case Success(_) => logger.info("Finish Success, yuu are the best!!!")
+        case Failure(exception) => logger.error("Feed error on: ", exception)
+      }
   }
-
-  system.scheduler.scheduleWithFixedDelay(5.seconds, 24.hour)(
-    saverScheduler
-  )
+  timer.scheduleAtFixedRate(saverScheduler, 3.seconds.toMillis, 10.seconds.toMillis)
 }
